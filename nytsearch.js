b2612c073da1f763b7c448d88177c7aa0852fd77
mnelson11 <mackenziernelson@gmail.com>
2017-07-27 21:47:05 -0400

    something works now

--- nytsearch.js
+++ nytsearch.js
@@ -1,27 +1,43 @@
 $(document).ready(function(){
 
 	$("#submitBtn").on("click", function(event){
-		console.log("clicked!");
-		var searchTerm = $("#searchParamater").val().trim();
+		var searchTerm = $("#searchParameter").val().trim();
 		var recordsNum = $("#numRecords").val().trim();
 		var yrStart = $("#startYear").val().trim();
 		var yrEnd = $("#endYear").val().trim();
 
-	 console.log(searchTerm+", "+recordsNum+", "+yrStart+", "+yrEnd);
-	});
+	 //console.log(searchTerm+", "+recordsNum+", "+yrStart+", "+yrEnd);
+	
 
-	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-	url += '?' + $.param({
-	 'api-key': "c4c53fe284df4db9993a75a5e5f03b9e"
-	});
-	$.ajax({
-	 url: url,
-	 method: 'GET',
-	}).done(function(result) {
-	}).fail(function(err) {
-	 throw err;
-	});
 
+
+
+		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+		url += '?' + $.param({
+		 'api-key': "c4c53fe284df4db9993a75a5e5f03b9e", 
+		 "fq":searchTerm
+		});
+		$.ajax({
+		 url: url,
+		 method: 'GET',
+		}).done(function(result) {
+			var finR= result.response;
+			// console.log(result);
+			//console.log(finR);
+			// var headline = finR.docs.headline
+
+			for(var i=0;i<finR.docs.length;i++){
+				console.log(finR.docs[i].headline);
+				$("#results").append(finR.docs[i].headline.main);
+			}
+			// finR.forEach(function(docs){
+			// 	$("#results").append(docs.headline);
+			// });
+			
+		}).fail(function(err) {
+		 throw err;
+		});
+	});
 
 
 });
